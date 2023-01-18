346962a84cc3a4c48f87293bf67e8b7c64111a6c
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-01-19 02:55:39 +0530

    Update bug

--- csv-file/src/main.rs
+++ csv-file/src/main.rs
@@ -1,5 +1,9 @@
 use csv;
 use std::error::Error;
+
+fn read_from_file(filename: &str) -> Result<(), Box<dyn Error>> {} // handle realtime errors
 fn main() {
-    if let Err(e) = read_from_file("./csv-file/maincsvforrust.csv") {}
+    if let Err(e) = read_from_file("./csv-file/maincsvforrust.csv") {
+        println!("{}", e); // println! fineshed
+    }
 }
