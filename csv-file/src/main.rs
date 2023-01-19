ef6a57eec754443dd61c13d18ab1e26924b73565
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-01-19 16:42:38 +0530

    Update commit

--- csv-file/src/main.rs
+++ csv-file/src/main.rs
@@ -3,6 +3,14 @@ use std::error::Error;
 
 fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
     let mut reader = csv::Reader::from_path(path)?;
+
+    for result in reader.records() {
+        let record = result?;
+
+        println!("{:?}", record);
+    }
+
+    ok(())
 } // handle realtime errors
 fn main() {
     if let Err(e) = read_from_file("./csv-file/maincsvforrust.csv") {
